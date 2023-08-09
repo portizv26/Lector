d2bd92c178c9c09142a3f568e520dd2b1a8519ac
portizv26 <patricio@coddi.ai>
2023-08-08 21:43:19 -0400

    funciones documentadas con comentarios

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 import shutil
 
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_type = os.getenv('OPENAI_API_TYPE')
 openai.api_version = os.getenv('OPENAI_API_VERSION')
@@ -11,98 +12,169 @@ openai.api_base = os.getenv('OPENAI_API_BASE')
 
 from src.utils import *
 
+# Basic text for the app
 st.title("Conversa con tus PDF's")
 st.write('''Aplicación en etapa de testeo para que puedan interactuar con sus documentos por medio de un chat.
 Actualmente se tiene la opción de crear nuevos modelos y cargar modelos creados con documentación ya subida.
 Cualquier duda respecto al uso de la herramienta contactar a Patricio Ortiz : patricio@coddi.ai''')
 
+# Define state variables
+## History of messages
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+## Name of the experiment used in the session 
 if "name_exp" not in st.session_state:
     st.session_state.name_exp = ""
 
+## System role used in the session 
+if "system_role" not in st.session_state:
+    st.session_state.system_role = ""
+
+## Validation of the experiment name
 if "exp_bool" not in st.session_state:
     st.session_state.exp_bool = False
 
+# Sidebar of the app
 with st.sidebar:
+    # Logo of the company
     st.image('img/logo.jpg')
+    # Main option of actions for the user
     action = st.selectbox("¿Que deseas hacer?", ("","Cargar un modelo existente","Crear un nuevo modelo", ))
 
+    # Case 1. Load a model
     if action == "Cargar un modelo existente":
+        # Get experiments in memory
         store_list = os.listdir('Storage')
-        # st.write(store_list)
+        # Select model
         exp_name = st.selectbox("Elige un experimento", [""] + store_list)
+        # SIf model is selected
         if exp_name != "":
+            # Save the name as state variable
             st.session_state.name_exp = exp_name
+            # Change the validation of the experiment to True
             st.session_state.exp_bool = True
-            get_personality(exp_name)
-            get_docs_uploaded(exp_name)
+            # Obtain a description of the experiment
+            st.session_state.system_role = get_personality(exp_name)
+            get_docs(exp_name)
 
+    # Case 2. Create a model
     elif action == "Crear un nuevo modelo":
+        # Help text for user.
         st.header("Personalice su experimento")
         st.write('A continuación usted podrá personalizar el modelo de IA con el que interactuará, entregando dos paramétros:')
         st.write('*Personalidad del modelo : Rol que la IA tomará. Este es un parámtero opcional, con el cual pueden lograr resultados más personalizados.')
         st.write('Ejemplos de este campo son: "ingeniero mecánico con experiencia en equipos mineros" o "directivo de empresa buscando mejorar la productividad"')
         st.write('*Nombre del experimento : Con el cual podrá cargar su modelo en el futuro.')
+
+        # user inputs to personalise the model.
         system_role = st.text_input("Personalidad del modelo", "")
         exp_name = st.text_input("Nombre del modelo", "")
 
+        # Validate the name provided by the user
         exp_bool = check_exp_name(exp_name, st.session_state.name_exp)
-        if exp_bool:            
-            system_bool = check_role(system_role, exp_name)
+
+        # If the name is valid
+        if exp_bool:
+            # Save the name of the experiment as state variable
             st.session_state.name_exp = exp_name
+
+            # If a role is provided save it as .txt
+            check_personality(system_role, st.session_state.name_exp)
+
+            # Save the role of the agent as state variable
+            st.session_state.system_role = system_role
+
+            # Module to upload the documents
             st.header('Suba sus PDFs aquí')
             uploaded_files = st.file_uploader("Upload your documents", type="pdf", accept_multiple_files=True)
+            
+            # If the user uploads documents
             if uploaded_files:
+                # Create a state value to validate the upload and avoid errors
                 if 'qa' not in st.session_state:
+                    # wheel to show the user that the documents are being processed
                     with st.spinner("Cargando documentos..."):
+                        # Destroy the temp folder if it exists
+                        # This could happen if a previous excecution of the app was interrupted
                         if 'temp' in os.listdir():
                             shutil.rmtree('temp')
-                        # shutil.rmtree('temp')
-                        
+                        # Create a temp repository to save the model
                         os.mkdir('temp')
+
+                        # Save each uploaded file to the 'temp' folder
                         for uploaded_file in uploaded_files:
                             # Save each uploaded file to the 'temp' folder
                             with open(os.path.join('temp', uploaded_file.name), "wb") as f:
                                 f.write(uploaded_file.getvalue())
-
-                        docs_uploaded(exp_name)
-
-                        vector_index = create_index('temp', exp_name)
-
+                        # Create the index for the model -> the agent
+                        vector_index = create_index('temp', st.session_state.name_exp)
+                        # Save the name of the documents provided into a text file.
+                        docs_uploaded(st.session_state.name_exp)
+                        # Set the qa state variable to True in order to avoid errors
                         st.session_state['qa'] = True
+                        # Once the model is created, set the state of the model to True
                         st.session_state.exp_bool = True
+                        # Delete the temp folder
                         shutil.rmtree('temp')
-                    
 
+# If a valid experiment is loaded
+# This could be triggered in both use cases.
 if st.session_state.exp_bool:
 
+    # Display the previous messages from the session messages
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+    # If the user send the message
     if prompt := st.chat_input("What is up?"):
-        chat_history = get_history(st.session_state.messages, system_role=system_role)
-        # st.write(st.session_state.messages)
-        agent = call_agent(chat_history=chat_history, exp_name=st.session_state.name_exp)
 
-        st.session_state.messages.append({"role": "user", "content": prompt})
-
+        # Display the message sent by the user
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
-            response = agent.chat(prompt)
-            # st.write(response)
-            full_response += response.response
-            full_response += f"\n {8*'_'}"
-            mdata = response.metadata
-            for ref in mdata.values():
-                string_mdata = f'\n\n\n doc: {ref["file_name"]} | page: {ref["page_label"]}'
-                full_response += string_mdata
-            message_placeholder.markdown(full_response)
-        st.session_state.messages.append({"role": "assistant", "content": full_response, "metadata": response.metadata})
+        # with st.spinner('Pensando respuesta...')
 
+        # Get the history of messages to sent to the model.
+        chat_history = get_history(st.session_state.messages, st.session_state.system_role)
+        
+        # Create the agent using the history of messages
+        agent = call_agent(chat_history=chat_history, exp_name=st.session_state.name_exp)
+
+        # Save the message sent by the user
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        st.session_state.answering = True
+
+        if st.session_state.answering:
+
+            with st.spinner('Pensando respuesta...'):
+                # get the response from the model
+                response = agent.chat(prompt)
+                # get the response content from the model
+                full_response = f"{response.response} \n {8*'_'}"
+                mdata = response.metadata
+                for ref in mdata.values():
+                    full_response += f'\n\n doc: {ref["file_name"]} | page: {ref["page_label"]}'
+
+            # add the response to the session messages
+             # st.write('hola')
+            # Display the response from the model
+            with st.chat_message("assistant"):
+                # set an empty space
+                message_placeholder = st.empty()
+                # display the response
+                message_placeholder.markdown(full_response)
+
+            st.session_state.messages.append({"role": "assistant", "content": full_response, "metadata": response.metadata})
+            st.session_state.answering = False
+            
+        # st.write('hola2')
+        
+        # Boton de reinicio de conversación.
+        # if len(st.session_state.messages) >= 2:
+        #     reset_button = st.button("Reiniciar conversación")
+        #     st.write(reset_button)
+        #     if reset_button:
+        #         st.session_state.messages = []
--- src/utils.py
+++ src/utils.py
@@ -29,169 +29,200 @@ from dotenv import load_dotenv
 import streamlit as st
 
 load_dotenv()
-# openai.api_key = st.secrets["OPENAI_API_KEY"]
-# openai.api_type = st.secrets['OPENAI_API_TYPE']
-# openai.api_version = st.secrets['OPENAI_API_VERSION']
-# openai.api_base = st.secrets['OPENAI_API_BASE']
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_type = os.getenv('OPENAI_API_TYPE')
 openai.api_version = os.getenv('OPENAI_API_VERSION')
 openai.api_base = os.getenv('OPENAI_API_BASE')
 
-# openai.api_key = os.getenv("API_KEY")
-# st.write(openai.api_key)
-
-def create_index(path, exp_name=''):
+def create_index(path, exp_name):
+    # Define constants.
     max_input = 4096
     tokens = 500
-    chunk_size_limit = 1000 #for LLM, we need to define chunk size
+    chunk_size_limit = 1000 
     chunk_overlap_ratio = 0.2
 
-    #define prompt
-    promptHelper = PromptHelper(context_window = max_input,
-                                num_output = tokens,
-                                chunk_overlap_ratio = chunk_overlap_ratio,
-                                chunk_size_limit = chunk_size_limit
+    # Initializing the PromptHelper with defined parameters.
+    promptHelper = PromptHelper(context_window=max_input,
+                                num_output=tokens,
+                                chunk_overlap_ratio=chunk_overlap_ratio,
+                                chunk_size_limit=chunk_size_limit
                                 )
-    #define LLM — there could be many models we can use, but in this example, let’s go with OpenAI model
+    # Initializing the LLMPredictor using the OPENAI model.
     llmPredictor = LLMPredictor(llm=AzureChatOpenAI(deployment_name='gpt-35-turbo',
                                                     temperature=0,
                                                     ),
                                 )
     # llmPredictor = LLMPredictor(llm=ChatOpenAI(temperature=0,
-    #                                                 ),
+    #                                            ),
     #                             )
+    
+    # Initializing the embedding model.
     embed_model = LangchainEmbedding(OpenAIEmbeddings())
 
-    #load data — it will take all the .pdf files, if there are more than 1
+    # Loading data from the provided path (this assumes it's a directory containing .pdf files).
     docs = SimpleDirectoryReader(path).load_data()
 
-    #create vector index
+    # Creating a vector index using the loaded documents and defined models.
     service_context = ServiceContext.from_defaults(llm_predictor=llmPredictor,
                                                    prompt_helper=promptHelper,
                                                    embed_model=embed_model,
                                                    chunk_size=chunk_size_limit,
                                                    chunk_overlap=chunk_size_limit*chunk_overlap_ratio,
-                                                   context_window = max_input,
-                                                   num_output = tokens,
+                                                   context_window=max_input,
+                                                   num_output=tokens,
                                                    )
+    # Setting the global service context.
     set_global_service_context(service_context)
 
+    # Creating the GPTVectorStoreIndex from the loaded documents.
     vectorIndex = GPTVectorStoreIndex.from_documents(documents=docs,
                                                      service_context=service_context,
                                                      show_progress=False,
                                                      )
-    
-    vectorIndex.storage_context.persist(persist_dir = f'Storage/{exp_name}')
+    # Persisting the created vector index to storage.
+    vectorIndex.storage_context.persist(persist_dir=f'Storage/{exp_name}')
 
     return vectorIndex
 
 def load_index(exp_name=''):
+    # Initialize a storage context based on the experiment name provided.
     storage_context = StorageContext.from_defaults(persist_dir = f'Storage/{exp_name}')
+    
+    # Load the index from the storage using the initialized context.
     index = load_index_from_storage(storage_context)
 
     return index
+    
 
-def call_agent(chat_history, exp_name=''):
+def call_agent(chat_history, exp_name):
+    # Check if experiment name is empty.
+    # This could happen if the user is trying to call an agent that has not been created yet.
     if exp_name == '':
         return None
     else:
-        index=load_index(exp_name=exp_name)
+        # Load the stored index based on the experiment name.
+        index = load_index(exp_name=exp_name)
+        
+        # Get the query engine for the loaded index.
         query_engine = index.as_query_engine()
 
+        # Initialize the chat engine with the query engine and chat history provided.
         chat_engine = CondenseQuestionChatEngine.from_defaults(
             query_engine=query_engine, 
-            # condense_question_prompt=custom_prompt,
             chat_history=chat_history,
             verbose=False
         )
         return chat_engine
-
-def get_history(session_list, system_role=""):
-    # import time
-    # t=time.time()
+    
+def get_role(system_role):
+    # If no role has been assigned set a deafult system role.
     if system_role == "":
         chat_history=[
                 ChatMessage(role="system", content="Eres un asistente muy útil. Responde en español.")
                 ]
     else:
+        # Use the provided system role for the message.
         chat_history=[
                 ChatMessage(role="system", content=f"Eres un {system_role.lower()}. Responde en español.")
                 ]
-    if len(session_list) > 0:
-        for i in session_list:
-            if i['role'] == 'user':
+    return chat_history
+
+def get_history(session_messages, system_role):
+    # Initialize chat history with the system role.
+    chat_history = get_role(system_role)
+
+    # If there's previous session messages, append it to the chat history.
+    if len(session_messages) > 0:
+        for message in session_messages:
+            # If the message is from a user append the role and the content
+            if message['role'] == 'user':
                 role = MessageRole.USER
-                chat_history.append(ChatMessage(role=role, content=i['content']))
-            elif i['role'] == 'assistant':
+                chat_history.append(ChatMessage(role=role, 
+                                                content=message['content']))
+            # If the message is from an assistant append the role, content and metadata
+            elif message['role'] == 'assistant':
                 role = MessageRole.ASSISTANT
-                chat_history.append(ChatMessage(role=role, content=i['content'], metadata=i['metadata']))
-        return chat_history
-    else:
-        return chat_history
+                chat_history.append(ChatMessage(role=role, 
+                                                content=message['content'], 
+                                                metadata=message['metadata']))
     
-def check_exp_name(exp_name, session_string):
-    exp_name_store = f'{exp_name}'
-    if session_string != "":
+    return chat_history
+    
+def check_exp_name(exp_name, session_exp_name):
+    '''
+    exp_name: Experiment name provided by user.
+    session_string: Experiment name of the streamlit session.
+    '''
+    
+    # Check value of the session_exp_name
+    # If the value is not empty, means that a agent is already assigned.
+    if session_exp_name != "":
         return True
+    # If no agent is assigned, check if of the exp_name provided by user is valid.
     else:
-        if exp_name_store in os.listdir('Storage'):
+        # If experiment name already exists in storage.
+        if exp_name in os.listdir('Storage'):
             st.error('El nombre de experimento ya existe, por favor elija otro.')
             return False
+        # If experiment name is empty.
         elif (exp_name == ''):
             st.error('Ingrese un nombre de experimento.')
             return False
         else:
             return True
         
-def check_role(system_role, exp_name=''):
-    if system_role == "":
-        return False
-    else:
-        save_personality(system_role, exp_name)
-        return True
-        
-def save_personality(system_role, exp_name=""):
-    try:
-        os.makedirs(f'Storage/{exp_name}')
-    except:
-        pass
-    f = open(f"Storage/{exp_name}/role.txt", "w")
-    f.write(system_role)
-    f.close()
+def get_docs(exp_name):       
 
-def get_personality(exp_name):
-    try:
+    # If docs file is on directory, write the documents on screen
+    if 'docs.txt' in os.listdir(f'Storage/{exp_name}'):
+        p = f'Storage/{exp_name}/docs.txt'
+        with open(p) as f:  
+            documents_uploaded = f.read()
+        st.write(f'{8*"____"}\n{documents_uploaded}')
+
+def get_personality(exp_name):      
+    # If role file is on directory, write the personality on screen and return it
+    if 'role.txt' in os.listdir(f'Storage/{exp_name}'):
         p = f'Storage/{exp_name}/role.txt'
         with open(p) as f:  
             contents = f.read()
         personality = contents.title()
         st.write(f'{8*"____"}\nLa personalidad del modelo es: {personality}')
-    except:
-        pass
+        return personality
+    else:
+        return ""
+        
+def check_personality(system_role, exp_name):
+    # If a role is provided, save it to storage.
+    if system_role != "":
+        # Create a directory for the experiment.
+        try:
+            os.makedirs(f'Storage/{exp_name}')
+        except:
+            pass
+        # Save the role to a text file.
+        f = open(f"Storage/{exp_name}/role.txt", "w")
+        f.write(system_role)
+        f.close()
 
 def docs_uploaded(exp_name):
+    # Make sure a directory exists for the model
+    ## This could be avoidable, but I have it just in case.
     try:
         os.makedirs(f'Storage/{exp_name}')
     except:
         pass
-
+    # Get the names of the documents uploaded, without the extension.
     docs = [i.removesuffix('.pdf') for i in os.listdir('temp')]
+    # Save the names into an easy string
     string = 'El modelo fue entrenado con los siguientes PDFs:'
     for doc in docs:
         string += f'\n\n{doc}'
-        
+    # Save the string to a text file. 
     f = open(f"Storage/{exp_name}/docs.txt", "w")
     f.write(string)
     f.close()
-
-def get_docs_uploaded(exp_name):
-    p = f'Storage/{exp_name}/docs.txt'
-    with open(p) as f:  
-        documents_uploaded = f.read()
-    st.write(f'{8*"____"}\n{documents_uploaded}')
-    
 
 #TODO LIST:
 # 1. Obtener la metadata de las respuestas -> done
@@ -203,7 +234,6 @@ def get_docs_uploaded(exp_name):
 # 6,5 -> Explicar que tiene modelo anterior. -> done
 # 7. logo -> done
 # 8. lógica para nombre de experimento -> done
-
 
 # # list of `ChatMessage` objects
 # custom_chat_history = [
